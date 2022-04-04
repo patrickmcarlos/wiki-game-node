d9f12254a30ac8f36882c46c4151cc6e02eeb949
Patrick Carlos <patrickmcarlos@hotmail.com>
2022-04-03 20:13:05 -0700

    chore: adds dotenv, uses environment variable for port

--- app.ts
+++ app.ts
@@ -5,23 +5,28 @@ import cookieParser from 'cookie-parser';
 import logger from 'morgan';
 import { baseRouter, usersRouter } from './src/routes';
 
+require('dotenv').config();
+
 const app = express();
 
+/** EXPRESS SETUP */
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+/** ROUTES */
 app.use('/', baseRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
+/** ERROR HANDLING */
+// 404
 app.use(function (req: Request, res: Response, next: NextFunction) {
     next(createError(404));
 });
 
-// error handler
+// Catch all
 app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
     // set locals, only providing error in development
     res.locals.message = err.message;
@@ -31,8 +36,9 @@ app.use(function (err: any, req: Request, res: Response, next: NextFunction) {
     res.json('error');
 });
 
+/** SERVER SETUP */
 app.set('port', process.env.PORT || 6000);
 
 app.listen(app.get('port'), () => {
-    console.log(`Express server listening on port ${app.get('port')}`);
+    console.log(`Express server listening on port ${process.env.PORT}`);
 });

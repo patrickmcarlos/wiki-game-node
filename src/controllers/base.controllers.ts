71e7be15c24784c1aecacda55664c26fd7e21a45
Patrick Carlos <patrickmcarlos@hotmail.com>
2022-04-09 23:43:31 -0700

    refactor: random change to test pr hooks

--- src/controllers/base.controllers.ts
+++ src/controllers/base.controllers.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 
 const get = (req: Request, res: Response) => {
-    res.json({ title: 'GET /' });
+    res.json({ title: 'Welcome to the The Wiki Game Server!' });
 };
 
 export default { get };
